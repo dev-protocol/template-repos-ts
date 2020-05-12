868a95471b59733365eb12927178099cd4509c47
aggre <hiroyuki.aggre@gmail.com>
2020-05-13 00:46:51 +0900

    change structure

--- sign/importAuthorizer.ts
+++
@@ -1,9 +0,0 @@
-import { always, F } from 'ramda'
-import { Authorizer } from '.'
-
-export const importAuthorizer = async (
-	id: string
-): Promise<Authorizer | (() => boolean)> =>
-	import(`../functions/authorizer/${id}`)
-		.then((e: { readonly default: Authorizer }) => e.default)
-		.catch(always(F))
